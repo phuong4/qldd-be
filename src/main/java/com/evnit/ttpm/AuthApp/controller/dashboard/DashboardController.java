a7586b7ea1adc6c66d08c6cb15d521b5fe5834d9
Đúng Quy Trình <84451438+phuong4@users.noreply.github.com>
2023-11-28 09:33:53 +0700

    Update SQL query for XuLySuCo

--- src/main/java/com/evnit/ttpm/AuthApp/controller/dashboard/DashboardController.java
+++ src/main/java/com/evnit/ttpm/AuthApp/controller/dashboard/DashboardController.java
@@ -7,9 +7,11 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import com.evnit.ttpm.AuthApp.service.dashboard.DashboardService;
 
+@PreAuthorize("isAuthenticated()")
 @RestController
 @RequestMapping("/api/dashboard/")
 public class DashboardController {
